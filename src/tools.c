1555400ee638cd6bf259e6c6c2574fdb3ecf61aa
acd407 <acd407@qq.com>
2025-03-31 15:27:16 +0800

    mov utils.{c,h} -> tools.{c,h}

--- src/tools.c
+++ src/tools.c
@@ -1,3 +1,4 @@
+#include <main.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,4 +22,24 @@ void format_storage_units (char *buf, double bytes) {
     } else {
         snprintf (buf, 6, "%4.2f%c", bytes, units[unit_idx]);
     }
+}
+
+void output () {
+    putchar ('[');
+    int output_modules_cnt = 0;
+    for (size_t i = 0; i < modules_cnt; i++) {
+        if (modules[i].output) {
+            if (output_modules_cnt > 0) {
+                putchar (',');
+                fputs (
+                    "{\"full_text\":\" \",\"separator\":false,"
+                    "\"separator_block_width\":0,\"markup\":\"pango\"},",
+                    stdout
+                );
+            }
+            fputs (modules[i].output, stdout);
+            output_modules_cnt++;
+        }
+    }
+    puts ("],"); // 需要一个换行，puts隐含了
 }
