0bdb5b65e2af9475259e6f4b15aa00ad38051b80
acd407 <acd407@qq.com>
2025-04-01 12:41:05 +0800

    add a union module_t.data

--- include/main.h
+++ include/main.h
@@ -6,11 +6,15 @@
 
 typedef struct {
     void (*update) (void);
-    void (*alter) (uint64_t);
-    char *output;
-    int *fds;     // 监听的fd,给派发器用,数组末尾为-1
-    uint64_t sec; // 一个timer的附加项，确定模块是否每秒更新
-    uint64_t state;
+    void (*alter) (uint64_t); // input 专用
+    char *output;             // 输出
+    int *fds;                 // 监听的fd,给派发器用,数组末尾为-1
+    uint64_t interval;        // 确定模块更新的时间间隔，0表示不随时间更新
+    uint64_t state;           // input 专用
+    union {                   // 模块内部数据
+        void *ptr;
+        uint64_t num;
+    } data;
 } module_t;
 
 extern module_t modules[];
